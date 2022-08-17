1d69eb07780244ba54c789fd3692abb82c208ea3
guillaume <reygner0224@gmail.com>
2022-08-18 01:56:55 +0200

    first push

---
+++ README.md
@@ -0,0 +1,64 @@
+
+# Twitter Bot Header
+
+Real-Time Header bot for Twitter
+
+![banner](images/banner.png)
+
+
+## How to setup for your Twitter
+
+Add a file '.env' and insert this line :
+
+```
+API_KEY=//VOTRE API KEY
+API_SECRET=//VOTRE API KEY SECRET
+ACCESS_TOKEN=//VOTRE ACCESS TOKEN
+ACCESS_SECRET=//VOTRE ACCESS TOKEN SECRET
+```
+
+In index.js, modify screen_name
+
+```
+const follower = await twitterClient.accountsAndUsers.usersShow({
+    screen_name: "VOTRE PSEUDO TWITTER SANS @"
+  });
+```
+
+To get the API keys, you need to [apply for a Twitter Developer account](https://developer.twitter.com/en/apply-for-access). It's free and take ~5 minutes.
+
+Lastly, add a banner template file with this specific name : twitter-banner.png.
+(make sur your file is a png image and have a size of 1500x500px)
+
+
+## Run the script
+
+Install dependencies:
+```
+yarn
+```
+or
+```
+npm install
+```
+
+Start the app:
+
+```
+node index.js
+```
+
+Keep it running and have fun!
+
+## How the script works
+
+[French Tutorial here](https://hot-jujube-90c.notion.site/Twitter-Bot-Header-eef6c49eee4c4762bca9e4a50aefd91e). 
+
+The script fetch new replies every 60 seconds (to avoid rate limit)
+
+
+## Most importantly
+
+Follow me on Twitter! [@guillaume_rygn](https://twitter.com/guillaume_rygn)
+
+Thanks!
---
+++ index.js
@@ -0,0 +1,274 @@
+
+
+const dotenv = require("dotenv");
+dotenv.config();
+const { TwitterClient } = require("twitter-api-client");
+const axios = require("axios");
+const sharp = require("sharp");
+const fs = require("fs");
+// to bypass heroku port issue
+const http = require("http");
+
+const twitterClient = new TwitterClient({
+  apiKey: process.env.API_KEY,
+  apiSecret: process.env.API_SECRET,
+  accessToken: process.env.ACCESS_TOKEN,
+  accessTokenSecret: process.env.ACCESS_SECRET,
+});
+
+async function get_followers() {
+
+  /*---------------UPDATE LOCATION PROFIL---------------------*/
+
+
+  const follower = await twitterClient.accountsAndUsers.usersShow({
+    screen_name: "VOTRE PSEUDO TWITTER SANS @"
+  });
+
+  let location = []
+
+  if(follower.followers_count< 100){
+    location.push("0 ")
+    let greencube = Math.floor(follower.followers_count/ 20)
+    console.log(greencube)
+    let cube = 0
+    if(greencube > 0){
+      for(let i = 0; i < greencube; i++){
+        location.push("🟩")
+        cube ++
+      }
+      if(follower.followers_count- (greencube * 20) > 0){
+        location.push("🟨")
+        cube ++
+
+        while(cube < 5){
+          location.push("⬜️")
+          cube ++
+        }
+      } else {
+        while(cube < 5){
+          location.push("⬜️")
+          cube ++
+        }
+      }
+    } else {
+      if(follower.followers_count/ 20 > 0){
+        location.push("🟨")
+        cube ++
+        while(cube < 5){
+          location.push("⬜️")
+          cube ++
+        }
+      } else {
+        while(cube < 5){
+          location.push("⬜️")
+          cube ++
+        }
+      }
+    }
+    location.push(" 100 followers")
+  } else if(follower.followers_count>= 100 && follower.followers_count< 1000){
+    location.push("100 ")
+
+    let greencube = Math.floor(follower.followers_count/ 200)
+    console.log(greencube)
+    let cube = 0
+    if(greencube > 0){
+      for(let i = 0; i < greencube; i++){
+        location.push("🟩")
+        cube ++
+      }
+      if(follower.followers_count- (greencube * 200) > 0){
+        location.push("🟨")
+        cube ++
+
+        while(cube < 5){
+          location.push("⬜️")
+          cube ++
+        }
+      } else {
+        while(cube < 5){
+          location.push("⬜️")
+          cube ++
+        }
+      }
+    } else {
+      if(follower.followers_count/ 200 > 0){
+        location.push("🟨")
+        cube ++
+        while(cube < 5){
+          location.push("⬜️")
+          cube ++
+        }
+      } else {
+        while(cube < 5){
+          location.push("⬜️")
+          cube ++
+        }
+      }
+    }
+    location.push(" 1k followers")
+
+    /*----------------*/ 
+
+} else if(follower.followers_count>= 1000){
+
+  let number = Math.floor(follower.followers_count/ 1000)
+
+  location.push(`${number}k `)
+  
+    let greencube = Math.floor((follower.followers_count - (number*1000))/ 200)
+    console.log(greencube)
+    let cube = 0
+    if(greencube > 0){
+      for(let i = 0; i < greencube; i++){
+        location.push("🟩")
+        cube ++
+      }
+      if(follower.followers_count- (greencube * 200) > 0){
+        location.push("🟨")
+        cube ++
+
+        while(cube < 5){
+          location.push("⬜️")
+          cube ++
+        }
+      } else {
+        while(cube < 5){
+          location.push("⬜️")
+          cube ++
+        }
+      }
+    } else {
+      if((follower.followers_count - (number*1000))/ 200 > 0){
+        location.push("🟨")
+        cube ++
+        while(cube < 5){
+          location.push("⬜️")
+          cube ++
+        }
+      } else {
+        while(cube < 5){
+          location.push("⬜️")
+          cube ++
+        }
+      }
+    }
+    location.push(` ${number + 1}k followers`)
+}
+
+  const update = await twitterClient.accountsAndUsers.accountUpdateProfile({
+    location: location.join("")
+  });
+
+
+
+
+
+/*---------------UPDATE PROFIL PICTURE---------------------*/
+  const followers = await twitterClient.accountsAndUsers.followersList({
+    count: 3,
+  });
+
+  const image_data = [];
+  let count = 0;
+
+  const get_followers_img = new Promise((resolve, reject) => {
+    followers.users.forEach((follower, index, arr) => {
+      process_image(
+        follower.profile_image_url_https,
+        `${follower.screen_name}.png`
+      ).then(() => {
+        const follower_avatar = {
+          input: `${follower.screen_name}.png`,
+          top: parseInt(`${380 + 300 * index}`),
+          left: 3950,
+        };
+        image_data.push(follower_avatar);
+        count++;
+        if (count === arr.length) resolve();
+      });
+    });
+  });
+
+  get_followers_img.then(() => {
+    draw_image(image_data);
+  });
+}
+
+async function process_image(url, image_path) {
+  await axios({
+    url,
+    responseType: "arraybuffer",
+  }).then(
+    (response) =>
+      new Promise(async (resolve, reject) => {
+        const rounded_corners = new Buffer.from(
+          '<svg><rect x="0" y="0" width="250" height="250" rx="125" ry="125"/></svg>'
+        );
+        resolve(
+          sharp(response.data)
+            .resize(250, 250)
+            .composite([
+              {
+                input: rounded_corners,
+                blend: "dest-in",
+              },
+            ])
+            .png()
+            .toFile(image_path)
+        );
+      })
+  );
+}
+
+
+async function draw_image(image_data) {
+  try {
+
+    await sharp("twitter-banner.png")
+      .composite(image_data)
+      .toFile("new-twitter-banner.png");
+
+    upload_banner(image_data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function upload_banner(files) {
+  try {
+    const base64 = fs.readFileSync("new-twitter-banner.png", {
+      encoding: "base64",
+    });
+    await twitterClient.accountsAndUsers
+      .accountUpdateProfileBanner({
+        banner: base64,
+      })
+      .then(() => {
+        console.log("Upload to Twitter done");
+        delete_files(files);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function delete_files(files) {
+  try {
+    files.forEach((file) => {
+      if (file.input.includes(".png")) {
+        fs.unlinkSync(file.input);
+        console.log("File removed");
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+get_followers();
+
+setInterval(() => {
+  get_followers();
+}, 60000);
